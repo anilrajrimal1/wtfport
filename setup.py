d6fd47f03f7f91d37b45d18ae8758fc9fa2bae48
anilrajrimal1 <anilrajrimal@gmail.com>
2025-05-22 11:11:35 +0545

    feat(setup): add setup.py for packaging and installation

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='wtfport',
+    version='1.0.0',
+    packages=['wtfport'],
+    entry_points={
+        'console_scripts': [
+            'wtfport = wtfport.cli:main',
+        ],
+    },
+    install_requires=[
+        'psutil',
+    ],
+)
