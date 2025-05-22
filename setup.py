a3ecfaa423c1962e4292ff09fbd468c266cf59c5
anilrajrimal1 <anilrajrimal@gmail.com>
2025-05-22 13:02:58 +0545

    fix(pipy): bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='wtfport-cli',
-    version='1.0.0',
+    version='1.0.1',
     packages=['wtfport'],
     entry_points={
         'console_scripts': [
