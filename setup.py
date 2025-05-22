fc3bbfe3aa8e0aac93d087ca32e4e5e8e3a7297e
anilrajrimal1 <anilrajrimal@gmail.com>
2025-05-22 16:33:35 +0545

    bump: release v1.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wtfport-cli',
-    version='1.0.3',
+    version='1.0.4',
     description='Check what process is using a port (CLI)',
     author='Anil Raj Rimal',
     author_email='anilrajrimal@gmail.com',
