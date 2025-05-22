46c51405c09b3095d8a23e8f26de7ba807eb8a5a
anilrajrimal1 <anilrajrimal@gmail.com>
2025-05-22 16:03:36 +0545

    fix(setup): correct pyproject.toml and setup.py for proper packaging

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='wtfport-cli',
-    version='1.0.2',
-    packages=['wtfport'],
+    version='1.0.3',
+    description='Check what process is using a port (CLI)',
+    author='Anil Raj Rimal',
+    author_email='anilrajrimal@gmail.com',
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
             'wtfport = wtfport.cli:main',
@@ -12,4 +15,10 @@ setup(
     install_requires=[
         'psutil',
     ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+    ],
+    python_requires='>=3.9',
 )
